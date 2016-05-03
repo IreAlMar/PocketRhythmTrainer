14e352d217d5128f4cff8eff24023ec85baf1489
Irene <irmartinrodr@gmail.com>
2016-05-03 11:12:42 +0200

    First commit

---
+++ app/src/main/java/com/irene/pocketrhythmtrainer/TempoSettings.java
@@ -0,0 +1,13 @@
+package com.irene.pocketrhythmtrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempoSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempo_settings);
+    }
+}
